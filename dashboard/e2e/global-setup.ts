f0f2dd1c2b783da6864063c70667aa00c4754d4d
Russell Teter <russellteter@github.com>
2026-05-04 14:11:23 -0400

    test(e2e): playwright config + dual webserver (fixture + next)

---
+++ dashboard/e2e/global-setup.ts
@@ -0,0 +1,7 @@
+// Global setup hook for Playwright. Reserved for future use:
+// fixture mutation, pre-test seeding, auth bootstrap, etc.
+async function globalSetup() {
+  return undefined;
+}
+
+export default globalSetup;
